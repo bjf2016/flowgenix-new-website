f0a1fdd88e4e0f46bff5da82e8e6b70cb6dad179
bjf2016 <ben.foroodian@yahoo.com>
2025-10-06 09:59:14 -0700

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from './Logo';
 
 export function Footer() {
   return (
-    <footer className="border-t bg-gray-50">
+    <footer className="border-t bg-[#1F2528]">
       <div className="container mx-auto max-w-7xl px-6 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1">
