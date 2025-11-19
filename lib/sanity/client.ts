c69bd5edee0e88234abd0068d91d8648738f4969
bjf2016 <ben.foroodian@yahoo.com>
2025-11-19 14:55:28 -0800

    Updated client.ts

--- lib/sanity/client.ts
+++ lib/sanity/client.ts
@@ -1,11 +1,14 @@
 import { createClient } from '@sanity/client';
 
+const projectId = process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
+const dataset = process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || '';
+
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
-  dataset: process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || '',
-  apiVersion: process.env.SANITY_API_VERSION || '2025-01-01',
+  projectId,
+  dataset,
+  apiVersion: process.env.SANITY_API_VERSION || '2024-05-01',
   token: process.env.SANITY_READ_TOKEN,
-  useCdn: true,
+  useCdn: false,
 });
 
 export const sanityClient = client;
